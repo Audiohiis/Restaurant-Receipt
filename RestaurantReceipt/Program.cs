1329e58e0de17496a8e3943386cf3d6ca0b40dd8
Hedi Tamm <hedi.tamm@bigbank.ee>
2017-11-20 13:39:52 +0200

    tunnikontroll

---
+++ RestaurantReceipt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RestaurantReceipt
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double userInput = 0;
+            double sum = 1;
+
+            Console.WriteLine("Taco Palenque");
+            Console.WriteLine("1200 Main ST.");
+            Console.WriteLine("------------------------------------------------");
+
+            while (true)
+            {
+                Console.Write("Enter price of food item [-1 to quit]: ");
+                userInput = double.Parse(Console.ReadLine());
+                sum += userInput;
+
+                if (userInput == -1)
+                {
+                    Console.WriteLine("------------------------------------------------");
+                    double gratuity = sum * 0.15;
+                    double gratuitySum = sum - gratuity;
+                    Console.WriteLine($"Subtotal: €{ sum }");
+                    Console.WriteLine($"15.00 % Gratuity: €{ gratuity }");
+                    Console.WriteLine($"Total: €{ gratuitySum }");
+                    break;
+                }
+            }
+        }
+    }
+}
